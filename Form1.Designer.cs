0580692f3a6ae0c9d6f584c055240c258ddf0314
dep try <cdung288@gmail.com>
2022-02-16 15:52:08 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace FilterMember
+{
+    partial class Zalo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.textGroupName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textPhoneNumber
+            // 
+            this.textPhoneNumber.Location = new System.Drawing.Point(95, 15);
+            this.textPhoneNumber.Name = "textPhoneNumber";
+            this.textPhoneNumber.Size = new System.Drawing.Size(202, 20);
+            this.textPhoneNumber.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Phone number";
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(95, 53);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.PasswordChar = '*';
+            this.textPassword.Size = new System.Drawing.Size(202, 20);
+            this.textPassword.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "        Password";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(273, 89);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(73, 25);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Status:";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(60, 95);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 13);
+            this.labelStatus.TabIndex = 7;
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.buttonRemove);
+            this.groupBox.Controls.Add(this.textGroupName);
+            this.groupBox.Controls.Add(this.label4);
+            this.groupBox.Location = new System.Drawing.Point(16, 133);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(330, 88);
+            this.groupBox.TabIndex = 8;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Remove members do not vote";
+            this.groupBox.Visible = false;
+            this.groupBox.VisibleChanged += new System.EventHandler(this.groupBox_VisibleChanged);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(255, 65);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 2;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // textGroupName
+            // 
+            this.textGroupName.Location = new System.Drawing.Point(79, 31);
+            this.textGroupName.Name = "textGroupName";
+            this.textGroupName.Size = new System.Drawing.Size(202, 20);
+            this.textGroupName.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Group name:";
+            // 
+            // Zalo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 121);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textPhoneNumber);
+            this.Name = "Zalo";
+            this.Text = "Zalo";
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textPhoneNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.TextBox textGroupName;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilterMember
+{
+    public partial class Zalo : Form
+    {
+        public Zalo()
+        {
+            InitializeComponent();
+        }
+
+        static ChromeDriver cDriver = null;
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--user-data-dir=" + Application.StartupPath + "\\Profiles\\0");
+
+            cDriver = new ChromeDriver(service, options);
+            
+            cDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            if(textPhoneNumber.Text.Length > 0 || textPassword.Text.Length > 0)
+            {
+                labelStatus.Text = "login...";
+                cDriver.Navigate().GoToUrl("https://id.zalo.me/account?continue=https://chat.zalo.me");
+                try
+                {
+                    cDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div[2]/div[2]/div[1]/ul/li[2]/a")).Click();
+                    cDriver.FindElement(By.CssSelector("#input-phone")).SendKeys(textPhoneNumber.Text);
+                    
+                }
+                catch (Exception)
+                { 
+                }
+
+                try
+                {
+                    cDriver.FindElement(By.CssSelector("input[type=password]")).SendKeys(textPassword.Text);
+                    cDriver.FindElement(By.XPath("//*[@id='app']/div/div/div[2]/div[2]/div[2]/div/div/div/div[4]/a")).Click();
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            
+            if (cDriver.Url.Contains("https://chat.zalo.me/"))
+            {
+                labelStatus.Text = "success";
+                groupBox.Visible = true;
+            }
+            else
+                labelStatus.Text = "failed";
+
+        }
+
+        private void groupBox_VisibleChanged(object sender, EventArgs e)
+        {
+            this.Size = new Size(this.Size.Width, this.Visible ? 272 : 160);
+
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            var groupList = cDriver.FindElements(By.CssSelector("#conversationList div.conv-item-title__name.truncate > span"));
+            foreach (var group in groupList)
+            {
+                if (group.GetAttribute("innerText").Contains(textGroupName.Text))
+                {
+                    group.Click();
+                    break;
+                }
+            }
+
+            try
+            {
+                var pin = cDriver.FindElement(By.CssSelector("#messageView div.content-title-container > span"));
+                if (pin.GetAttribute("innerText") == "Bình chọn")
+                {
+                    cDriver.FindElement(By.CssSelector("#header > div.threadChat.group div.subtitle__groupmember__content.clickable > div")).Click();
+
+                    List<String> memberList = new List<string>();
+
+                    foreach (var member in cDriver.FindElements(By.CssSelector("#member-group div.zl-avatar.chat-box-member__info__avatar.v2.zl-avatar--m > img")))
+                    {
+                        memberList.Add(member.GetAttribute("src"));
+                    }
+
+                    memberList.RemoveAt(0);
+                    cDriver.FindElement(By.XPath("/html/body/div/div/div[2]/main/aside/div[2]/div[1]/div/div/div")).Click();
+                    pin.Click();
+
+                    cDriver.FindElement(By.CssSelector("#group-poll-create-content > div.group-poll-vote-description.href.underline")).Click();
+
+                    foreach (var member in cDriver.FindElements(By.CssSelector(".group-poll-detail img.zl-avatar__photo, .group-poll-detail zl-avatar__alphabet")))
+                    {
+                        memberList.Remove(member.GetAttribute("src"));
+                    }
+
+                    cDriver.FindElement(By.CssSelector("div.zl-modal__dialog__header i")).Click();
+                    cDriver.FindElement(By.CssSelector("div.zl-modal__dialog__header i")).Click();
+                    cDriver.FindElement(By.CssSelector("#header > div.threadChat.group div.subtitle__groupmember__content.clickable > div")).Click();
+
+                    IJavaScriptExecutor executor = cDriver as IJavaScriptExecutor;
+
+                    foreach (var member in memberList)
+                    {
+                        Thread.Sleep(200);
+                        try
+                        {
+                            executor.ExecuteScript("document.querySelector(`#member-group img[src='" + member + "']`).parentElement.parentElement.querySelector(`.fa`).click()");
+
+                        }
+                        catch (Exception)
+                        {
+                            executor.ExecuteScript("document.querySelector(`#member-group img[src='" + member.Substring(6) + "']`).parentElement.parentElement.querySelector(`.fa`).click()");
+                        }
+                        cDriver.FindElement(By.CssSelector("body > div.popover-v3 > div.zmenu-body > div")).Click();
+                        cDriver.FindElement(By.CssSelector("div.zl-modal__footer div.z--btn.z--btn--fill--primary.-lg.--rounded.zl-modal__footer__button > div")).Click();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+
+    
+
+}
